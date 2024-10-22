9b95533ea38f34b8f6c1fc73d90a05e5c8fbea14
basirul billah <basirulbillah@gmail.com>
2024-10-22 15:20:09 +0600

    footer component added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import Orders from './pages/Orders';
 import PlaceOrder from './pages/PlaceOrder';
 import Product from './pages/Product';
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -27,6 +28,7 @@ const App = () => {
           <Route path="/place-order" element={<PlaceOrder />} />
           <Route path="/product/:productId" element={<Product />} />
         </Routes>
+        <Footer />
       </h1>
     </div>
   )
